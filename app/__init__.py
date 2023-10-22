9753aeb303f7c226128f800609bf316b0716db5a
kstinglhammer <k.stinglhammer@web.de>
2023-10-22 23:45:57 +0200

    Fixed issue with config import

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import config
+from .config import Config
 
 # Create the Flask app instance
 app = Flask(__name__, template_folder='../templates')
 
 # Configure the app
-app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 
 # Initialize the database
 db = SQLAlchemy(app)
